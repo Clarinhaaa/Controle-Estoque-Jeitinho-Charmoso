0ad9e4fa03128b3a0490dafd7ff862e2632ddb4b
Eduardo Neves <esnr@cin.ufpe.br>
2026-05-12 20:27:50 -0300

    feat: implementa insert, update e getById no ConjuntoDao e atualiza ConjuntoView

--- dao/ConjuntoDao.cpp
+++ dao/ConjuntoDao.cpp
@@ -10,12 +10,59 @@ QList<ConjuntoModel*> ConjuntoDao::getAll()
     query.exec();
     while (query.next()) {
         ConjuntoModel* conjunto = new ConjuntoModel();
-        conjunto->setId(query.value(0).toInt());
-        conjunto->setNome(query.value(1).toString());
-        conjunto->setEstoque(query.value(2).toInt());
-        conjunto->setPreco(query.value(3).toFloat());
+        conjunto->setId(query.value("id_conjunto").toInt());
+        conjunto->setNome(query.value("nome_conjunto").toString());
+        conjunto->setEstoque(query.value("estoque_conjunto").toInt());
+        conjunto->setPreco(query.value("preco_conjunto").toFloat());
         listConjunto.append(conjunto);
     }
 
     return listConjunto;
+}
+
+// busca um conjunto pelo ID no banco
+ConjuntoModel* ConjuntoDao::getById(int id)
+{
+    QSqlQuery query;
+    query.prepare("SELECT * FROM Conjunto WHERE id_conjunto = :id;");
+    query.bindValue(":id", id);
+
+    if (query.exec() && query.next()) {
+        ConjuntoModel* conjunto = new ConjuntoModel();
+        conjunto->setId(query.value("id_conjunto").toInt());
+        conjunto->setNome(query.value("nome_conjunto").toString());
+        conjunto->setEstoque(query.value("estoque_conjunto").toInt());
+        conjunto->setPreco(query.value("preco_conjunto").toFloat());
+        return conjunto;
+    }
+
+    // retorna nullptr se o ID não existir no banco
+    return nullptr;
+}
+
+// insere um novo conjunto no banco
+bool ConjuntoDao::insert(ConjuntoModel* conjunto)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO Conjunto (nome_conjunto, estoque_conjunto, preco_conjunto) "
+                  "VALUES (:nome, :estoque, :preco);");
+    query.bindValue(":nome", conjunto->getNome());
+    query.bindValue(":estoque", conjunto->getEstoque());
+    query.bindValue(":preco", conjunto->getPreco());
+
+    return query.exec();
+}
+
+// atualiza um conjunto existente no banco pelo ID
+bool ConjuntoDao::update(ConjuntoModel* conjunto)
+{
+    QSqlQuery query;
+    query.prepare("UPDATE Conjunto SET nome_conjunto = :nome, estoque_conjunto = :estoque, "
+                  "preco_conjunto = :preco WHERE id_conjunto = :id;");
+    query.bindValue(":nome", conjunto->getNome());
+    query.bindValue(":estoque", conjunto->getEstoque());
+    query.bindValue(":preco", conjunto->getPreco());
+    query.bindValue(":id", conjunto->getId());
+
+    return query.exec();
 }
--- dao/ConjuntoDao.h
+++ dao/ConjuntoDao.h
@@ -11,7 +11,14 @@ public:
     ConjuntoDao() {}
     ~ConjuntoDao() {}
 
+    // retorna todos os conjuntos cadastrados
     QList<ConjuntoModel*> getAll();
+    // retorna um conjunto pelo ID, ou nullptr se não encontrar
+    ConjuntoModel* getById(int id);
+    // insere um novo conjunto no banco
+    bool insert(ConjuntoModel* conjunto);
+    // atualiza um conjunto existente no banco
+    bool update(ConjuntoModel* conjunto);
 };
 
 #endif // CONJUNTODAO_H
